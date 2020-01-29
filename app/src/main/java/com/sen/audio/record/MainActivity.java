761f65485167d32c5a426c4c46637e93a91231ea
wangshengxing <www.sheng.xing@gmail.com>
2020-01-29 12:57:40 +0800

    init project

---
+++ app/src/main/java/com/sen/audio/record/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sen.audio.record;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
